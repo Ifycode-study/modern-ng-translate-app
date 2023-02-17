791fc9ea0c0a010b0c0ae07110249933407c1494
Ifycode <marychord@gmail.com>
2023-02-17 21:38:57 +0100

    Add translate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from "./translate.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-1-translate-app';
+
+  text!: string;
+  translatedText!: string;
+  value: any;
+
+  constructor(private translateService: TranslateService) {}
+
+  fromEvent(event: Event): string {
+    this.value = (event.target as HTMLInputElement).value;
+    return this.value;
+  }
+
+  submit() {
+    // console.log works
+    console.log(this.value, this.text);
+
+    // The Translate API given to use returns 403 forbidden error
+    this.translateService.translate(this.text).subscribe((result: string) => {
+      this.translatedText = result;
+    });
+  }
 }
